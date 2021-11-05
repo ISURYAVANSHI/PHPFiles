0c746c64a1a1983c2b3f6f1b3305e8407292e9b3
ISURYAVANSHI <92204581+ISURYAVANSHI@users.noreply.github.com>
2021-11-06 02:51:20 +0800

    Update MyPhotos.php

--- MyPhotos.php
+++ MyPhotos.php
@@ -1,1 +1,16 @@
-hi
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>HTML Form</title>
+  </head>
+  <body>
+    <h1>Form</h1>
+    <form method="POST" action="MyPhotos.php" enctype="multipart/form-data" >
+      <label  class="form-label">Name</label>
+      <input type="text" class="form-control" id="exampleInputEmail1" name="name" required > <br><br>
+      file : <input type="file" name="file"><br><br>
+      <br/>
+      <input type="submit" value="submit"  name="submit">
+    </form>
+  </body>
+</html>
