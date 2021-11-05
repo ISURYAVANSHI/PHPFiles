1eb64721249edf2146d3d5ea8ca65911512a6b02
ISURYAVANSHI <92204581+ISURYAVANSHI@users.noreply.github.com>
2021-11-06 07:38:45 +0800

    Update MyPhotos.php

--- MyPhotos.php
+++ MyPhotos.php
@@ -21,150 +21,86 @@
     </tr>
   </table>
 </form>
-<?php>
+
+
+<?php
+require_once 'vendor/autoload.php';
+
+
 use MicrosoftAzure\Storage\Blob\BlobRestProxy;
 use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
 use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
 use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
 use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
 
-//define('ACCOUNT_NAME', 'peachess');
-//define('ACCOUNT_KEY', 'uG9PwV9CDNsqLhZal2NQS5FaFy+tj3Yl8NIRVtTdwwOMmTo4+VtkDciAn9EsyNKgjhoUS+qzc2jRRqrYmx8k0Q==');
+$connectionString ="DefaultEndpointsProtocol=https;AccountName=peachess;AccountKey=uG9PwV9CDNsqLhZal2NQS5FaFy+tj3Yl8NIRVtTdwwOMmTo4+VtkDciAn9EsyNKgjhoUS+qzc2jRRqrYmx8k0Q==";
 
-//$connectionString = "DefaultEndpointsProtocol=https;AccountName=".getenv('ACCOUNT_NAME').";AccountKey=".getenv('ACCOUNT_KEY');
-
-$connectionString = DefaultEndpointsProtocol=https;AccountName=peachess;AccountKey=uG9PwV9CDNsqLhZal2NQS5FaFy+tj3Yl8NIRVtTdwwOMmTo4+VtkDciAn9EsyNKgjhoUS+qzc2jRRqrYmx8k0Q==;EndpointSuffix=core.windows.net;
-
-// Create blob client.
 $blobClient = BlobRestProxy::createBlobService($connectionString);
-
-$fileToUpload = "/Users/peachess/Documents/HelloWorld.txt";
+$fileToUpload = "/var/www/html/index.php";
 
 if (!isset($_GET["Cleanup"])) {
-    // Create container options object.
+  
 
 
-      $containerName = "https://peachess.blob.core.windows.net/myphotos";
+      $containerName = "myphotos";
 
     try {
-        // Create container.
-        //$blobClient->createContainer($containerName, $createContainerOptions);
-
-        // Getting local file so that we can upload it to Azure
+        
         $myfile = fopen($fileToUpload, "w") or die("Unable to open file!");
         fclose($myfile);
         
-        # Upload file as a block blob
+ 
         echo "Uploading BlockBlob: ".PHP_EOL;
         echo $fileToUpload;
-        echo "<br />";
+        echo "<br/>";
         
         $content = fopen($fileToUpload, "r");
 
-        //Upload blob
+ 
         $blobClient->createBlockBlob($containerName, $fileToUpload, $content);
 
-        // List blobs.
+
         $listBlobsOptions = new ListBlobsOptions();
-        $listBlobsOptions->setPrefix("HelloWorld");
+        $listBlobsOptions->setPrefix("index");
 
         echo "These are the blobs present in the container: ";
-
         do{
             $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
             foreach ($result->getBlobs() as $blob)
             {
-                echo $blob->getName().": ".$blob->getUrl()."<br />";
+                echo $blob->getName().": ".$blob->getUrl()."<br/>";
             }
         
             $listBlobsOptions->setContinuationToken($result->getContinuationToken());
         } while($result->getContinuationToken());
-        echo "<br />";
+        echo "<br/>";
 
-        // Get blob.
+     
         echo "This is the content of the blob uploaded: ";
         $blob = $blobClient->getBlob($containerName, $fileToUpload);
         fpassthru($blob->getContentStream());
-        echo "<br />";
-      
-      
-      <form action="<?PHP echo $_SERVER['MyPhotos.php'] ?>" method="POST">
-  <table border="0">
-    <tr>
-      <td>NAME1</td>
-      <td>ADDRESS1</td>
-    </tr>
-    <tr>
-      <td>
-        <input type="text" name="NAME1" maxlength="45" size="30" />
-      </td>
-      <td>
-        <input type="text" name="ADDRESS1" maxlength="90" size="60" />
-      </td>
-      <td>
-        <input type="submit1" value="Add Data" />
-      </td>
-    </tr>
-  </table>
-</form>
-      
+        echo "<br/>";
     }
     catch(ServiceException $e){
-        // Handle exception based on error codes and messages.
-        // Error codes and messages are here:
-        // http://msdn.microsoft.com/library/azure/dd179439.aspx
+  
         $code = $e->getCode();
         $error_message = $e->getMessage();
-        echo $code.": ".$error_message."<br />";
+        echo $code.": ".$error_message."<br/>";
     }
     catch(InvalidArgumentTypeException $e){
-        // Handle exception based on error codes and messages.
-        // Error codes and messages are here:
-        // http://msdn.microsoft.com/library/azure/dd179439.aspx
+
         $code = $e->getCode();
         $error_message = $e->getMessage();
-        echo $code.": ".$error_message."<br />";
+        echo $code.": ".$error_message."<br/>";
+    }
+    catch(InvalidArgumentTypeException $e){
+
+        $code = $e->getCode();
+        $error_message = $e->getMessage();
+        echo $code.": ".$error_message."<br/>";
     }
 } 
-else 
-{
-
-    try{
-        // Delete container.
-      
-      <form action="<?PHP echo $_SERVER['MyPhotos.php'] ?>" method="POST">
-  <table border="0">
-    <tr>
-      <td>NAME2</td>
-      <td>ADDRESS2</td>
-    </tr>
-    <tr>
-      <td>
-        <input type="text" name="NAME2" maxlength="45" size="30" />
-      </td>
-      <td>
-        <input type="text" name="ADDRESS2" maxlength="90" size="60" />
-      </td>
-      <td>
-        <input type="submit2" value="Add Data" />
-      </td>
-    </tr>
-  </table>
-</form>
-        echo "Deleting Container".PHP_EOL;
-        echo $_GET["containerName"].PHP_EOL;
-        echo "<br />";
-        $blobClient->deleteContainer($_GET["containerName"]);
-    }
-    catch(ServiceException $e){
-        // Handle exception based on error codes and messages.
-        // Error codes and messages are here:
-        // http://msdn.microsoft.com/library/azure/dd179439.aspx
-        $code = $e->getCode();
-        $error_message = $e->getMessage();
-        echo $code.": ".$error_message."<br />";
-    }
-}
 ?>
 </body>
 </html>
+
