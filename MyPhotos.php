e9d76b25210bf49c90d8f6aeeb270d97e0bbc261
ISURYAVANSHI <92204581+ISURYAVANSHI@users.noreply.github.com>
2021-11-06 03:14:58 +0800

    Update MyPhotos.php

--- MyPhotos.php
+++ MyPhotos.php
@@ -8,7 +8,7 @@
     <form method="POST" action="MyPhotos.php" enctype="multipart/form-data" >
       <label  class="form-label">Name</label>
       <input type="text" class="form-control" id="exampleInputEmail1" name="name" required > <br><br>
-      file : <input type="file" name="file"><br><br>
+      <input type="file" name="file"><br><br>
       
       <input type="submit" value="submit"  name="submit">
     </form>
